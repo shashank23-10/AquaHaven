085d27c6252cfb484f73fa23f5af9d3c587fa58b
shashank pandey <shashankpandey2001@gmail.com>
2025-03-18 07:50:02 +0530

    Update 1.1.1

--- fish-frontend/src/App.js
+++ fish-frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Helmet } from "react-helmet";
 import HomePage from "./components/HomePage";
 import Fishes from "./components/Fishes";
 import Plants from "./components/Plants";
@@ -10,6 +11,10 @@ import DIYs from "./components/DIYs";
 function App() {
   return (
     <Router>
+      <Helmet>
+        <title>HydroSanctuary - Sustainable Water Sanctuaries for Home & Nature</title>
+        <meta name="description" content="Discover HydroSanctuary – your guide to creating eco-friendly water sanctuaries, sustainable aquaculture, and aquarium care." />
+      </Helmet>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/fishes" element={<Fishes />} />
